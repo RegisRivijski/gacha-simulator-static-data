bf8af2ac1defe4c5986246cbec9dede9250e727c
Artur Petrov <artur.petrov@auto.ria.com>
2022-10-18 02:17:18 +0300

    app/index.js update

--- app/index.js
+++ app/index.js
@@ -1,17 +1,13 @@
-const util = require('util');
-
 const itemsData = require('./processors/itemsData');
 const imagesData = require('./processors/itemsImages');
 const finalGachaImages = require('./processors/finalGachaImages');
-
-const consoleInfoPromise = util.promisify(console.info);
 
 async function main() {
   await itemsData.generate();
   await imagesData.generate();
   await finalGachaImages.generate();
 
-  await consoleInfoPromise('Done!');
+  console.info('Done!');
 }
 
 main();
